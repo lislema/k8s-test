1e121dae49c301dc27b3bbfcd30d0340a0a720ce
lislema <mark.lisle@gmail.com>
2024-12-02 11:40:24 +0100

    adding permissions for autoscaling

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,6 +139,11 @@ module "eks" {
       min_size     = 1
       max_size     = 10
       desired_size = 2
+
+      tags = {
+        "k8s.io/cluster-autoscaler/enabled"               = "true"
+        "k8s.io/cluster-autoscaler/${var.cluster_name}"  = "owned"
+      }
     }
 
     two = {
@@ -149,8 +154,71 @@ module "eks" {
       min_size     = 1
       max_size     = 10
       desired_size = 1
+
+      tags = {
+        "k8s.io/cluster-autoscaler/enabled"               = "true"
+        "k8s.io/cluster-autoscaler/${var.cluster_name}"  = "owned"
+      }
     }
   }
+}
+
+# IAM role for Autoscaling 
+resource "aws_iam_role" "eks_node_group" {
+  name = "${var.cluster_name}-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+  ])
+
+  role       = aws_iam_role.eks_node_group.name
+  policy_arn = each.value
+}
+
+# Add Cluster Autoscaler-specific policy
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name        = "${var.cluster_name}-cluster-autoscaler-policy"
+  description = "IAM policy for Cluster Autoscaler"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeTags",
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+          "ec2:DescribeLaunchTemplateVersions"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler_policy" {
+  role       = aws_iam_role.eks_node_group.name
+  policy_arn = aws_iam_policy.cluster_autoscaler.arn
 }
 
 # with EKS you need the EBS CSI driver to allow for EBS volumes to be mounted to pods!!!
